dbd73189c9f9612ed8bd6523d08d17a59f629567
pchandt <pcthakuri26@gmail.com>
2022-07-27 22:15:25 -0700

    Implementation and tests: should be complete!

--- src/main/java/com/tlglearning/Square.java
+++ src/main/java/com/tlglearning/Square.java
@@ -25,7 +25,7 @@ public final class Square {
    * @throws IllegalArgumentException When {@code input} is negative.
    */
   public static boolean isPerfectSquare(long input) throws IllegalArgumentException {
-    if(input < 0){
+    if (input < 0) {
       throw new IllegalArgumentException(String.format(ILLEGAL_INPUT_FORMAT, input));
 
     }
